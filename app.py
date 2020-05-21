fcfa0171ea2a4dfc0c8e487df0f4ebe5466cf679
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-21 12:10:59 +0200

    Add Test to select correct cluster in Knn function

--- app.py
+++ app.py
@@ -84,9 +84,12 @@ def KnnPrediction(df_Movies,df_movie_id):
     model_KNN.fit(X,y)
 
     MovieTemp = model_KNN.kneighbors(df_inter.loc[df_inter['tconst']==movie_id, columns],n_neighbors=6)
-    st.write("MovieTemp")
-    st.write(MovieTemp)
-    return MovieTemp[1:6]
+    
+    df_Cluster = pd.DataFrame()
+    for i in range(1,6):
+        df_Cluster.append(df_inter.iloc[MovieTemp[1][0][i]], ignore_index=True)
+    st.dataframe(df_Cluster)
+    return df_Cluster
 
 def main():
 
