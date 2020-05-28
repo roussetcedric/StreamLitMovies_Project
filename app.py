5552d5ca142d1e489b5b2e0c271e4c774161c60a
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-28 16:50:05 +0200

    Correct Total Time screening

--- app.py
+++ app.py
@@ -292,9 +292,9 @@ def main():
             captionList = []
             st.write("* **5 Derniers films vus** :")
             for loop in range(1,5) :
-                st.write(df_Analysis.loc[loop]["tconst"])
+                st.write(df_Analysis.loc[loop])
                 #picList.append(str(get_poster_from_api(df_Analysis[loop]["tconst"])))
-                st.write(df_Analysis.loc[loop]["originalTitle"])
+                #st.write(df_Analysis.loc[loop]["originalTitle"])
                 #captionList.append(str(df_Analysis[loop]["originalTitle"]))
             #st.image(picList, width=100, caption=captionList)
 
