9f805b8172f8ed04cdff92360721f6c25a589eb2
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-28 17:45:06 +0200

    Correct Total Time screening

--- app.py
+++ app.py
@@ -270,7 +270,8 @@ def main():
                     df_Analysis['genres'].str.get_dummies(sep=','),
                     df_Analysis['averageRating']],
                     axis=1)
-        df_GenresMultiply = df_Genres*df_Genres["averageRating"]
+        for col in df_Genres.columns :
+            df_GenresMultiply = df_Genres[col]*df_Genres["averageRating"]
         st.write(df_GenresMultiply)
 
         df_GenrePie = pd.melt(df_Genres,value_vars=df_Genres.columns[1:-1],var_name='Genre',value_name='Nombre').groupby('Genre').sum()
@@ -298,10 +299,9 @@ def main():
             st.write("* **5 Derniers films vus** :")
             df_Analysis = df_Analysis.reset_index(drop=True)
             st.write(df_Analysis)
-            for loop in range(1,5) :
-                st.write(loop)
-                #st.write(df_Analysis[loop]["tconst"])
-                #st.write(df_Analysis[loop]["tconst"].iloc[0])
+            for loop in range(1,6) :
+                st.write(df_Analysis[-loop]["tconst"])
+                st.write(df_Analysis[-loop]["tconst"].iloc[0])
                 #picList.append(get_poster_from_api(df_Analysis[loop]["tconst"]))
                 #captionList.append(df_Analysis[loop]["originalTitle"])
             #st.image(picList, width=100, caption=captionList)
