40ebdf2169c7fb99d7cea6ab1ec5633c286b61e9
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-28 15:57:29 +0200

    Correct Total Time screening

--- app.py
+++ app.py
@@ -293,10 +293,10 @@ def main():
             st.write("* **5 Derniers films vus** :")
             for loop in range(1,5) :
                 st.write(df_Analysis[loop]["tconst"])
-                picList.append(str(get_poster_from_api(df_Analysis[loop]["tconst"])))
+                #picList.append(str(get_poster_from_api(df_Analysis[loop]["tconst"])))
                 st.write(df_Analysis[loop]["originalTitle"])
-                captionList.append(str(df_Analysis[loop]["originalTitle"]))
-            st.image(picList, width=100, caption=captionList)
+                #captionList.append(str(df_Analysis[loop]["originalTitle"]))
+            #st.image(picList, width=100, caption=captionList)
 
     elif AdminitrationPage == "Utilisateur" :
 
