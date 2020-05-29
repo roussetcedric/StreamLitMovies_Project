ba138cdefdc5449b1d7937c704b39d7f3c66471a
roussetcedric <59645699+roussetcedric@users.noreply.github.com>
2020-05-29 13:16:37 +0200

    Change BackGround Function

--- app.py
+++ app.py
@@ -355,7 +355,10 @@ def main():
                         "}" \
                         "</style>" \
                         "\"\"\""
-                    st.markdown(BackGround_string)
+                    #st.markdown(BackGround_string, unsafe_allow_html=True)
+
+                    BackGround_string2 = "<style> body\{background: url('" + get_poster_from_api(df_MovieSelectedOne.iloc[0]["tconst"]) + "') center center no-repeat;background-size: cover;background-attachment: fixed;color: white;height: 100%;width: 100%;\}</style>"
+                    st.markdown(BackGround_string2, unsafe_allow_html=True)
                 else :
                     st.markdown("""
                     <style>
